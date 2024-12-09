d135d7d55be92c0801006f4cca539266c9268595
Alexutzu132 <102051631+Alexutzu132@users.noreply.github.com>
2024-12-09 11:04:54 +0200

    Update index2.js

--- index2.js
+++ index2.js
@@ -42,7 +42,7 @@
                 } else if (raspuns==="15"){
                         confirm("The vinnie's boyfriend")
                 } else if (raspuns==="secret"){
-                        document.getElementById('Screenshot_20240407_221839_WhatsApp.jpg').style.display="block"
+                        document.getElementById('secret').style.display="block"
                 }
     } else if (answer==="playlist"){
         document.getElementById('playlist').style.display="block"
